211365408cb9c14937aa9ad2a8a47e2699c0dcff
Luca Pinchetti <luca.pinchetti@cs.ox.ac.uk>
2024-11-12 18:34:14 -0800

    Fix test on linux

--- tests/_storage/_namespace_test.py
+++ tests/_storage/_namespace_test.py
@@ -9,9 +9,10 @@ from fast_graphrag._storage._namespace import Namespace, Workspace
 
 
 class TestWorkspace(unittest.IsolatedAsyncioTestCase):
-
     def setUp(self):
-        def _(self: Workspace) -> None: pass
+        def _(self: Workspace) -> None:
+            pass
+
         Workspace.__del__ = _
         self.test_dir = "test_workspace"
         self.workspace = Workspace(self.test_dir)
@@ -36,21 +37,25 @@ class TestWorkspace(unittest.IsolatedAsyncioTestCase):
         for checkpoint in [1, 2, 3]:
             os.makedirs(os.path.join(self.test_dir, str(checkpoint)))
         self.workspace = Workspace(self.test_dir)
+
         async def sample_fn():
             if "1" not in cast(str, self.workspace.get_load_path()):
                 raise Exception("Checkpoint not loaded")
             return "success"
+
         result = await self.workspace.with_checkpoints(sample_fn)
         self.assertEqual(result, "success")
         self.assertEqual(self.workspace.current_load_checkpoint, 1)
-        self.assertEqual(self.workspace.failed_checkpoints, ['3', '2'])
+        self.assertEqual(self.workspace.failed_checkpoints, ["3", "2"])
 
     async def test_with_checkpoint_no_failure(self):
         for checkpoint in [1, 2, 3]:
             os.makedirs(os.path.join(self.test_dir, str(checkpoint)))
         self.workspace = Workspace(self.test_dir)
+
         async def sample_fn():
             return "success"
+
         result = await self.workspace.with_checkpoints(sample_fn)
         self.assertEqual(result, "success")
         self.assertEqual(self.workspace.current_load_checkpoint, 3)
@@ -60,18 +65,20 @@ class TestWorkspace(unittest.IsolatedAsyncioTestCase):
         for checkpoint in [1, 2, 3]:
             os.makedirs(os.path.join(self.test_dir, str(checkpoint)))
         self.workspace = Workspace(self.test_dir)
+
         async def sample_fn():
             raise Exception("Checkpoint not loaded")
 
         with self.assertRaises(InvalidStorageError):
             await self.workspace.with_checkpoints(sample_fn)
         self.assertEqual(self.workspace.current_load_checkpoint, None)
-        self.assertEqual(self.workspace.failed_checkpoints, ['3', '2', '1'])
+        self.assertEqual(self.workspace.failed_checkpoints, ["3", "2", "1"])
 
     async def test_with_checkpoint_all_failures_accept_none(self):
         for checkpoint in [1, 2, 3]:
             os.makedirs(os.path.join(self.test_dir, str(checkpoint)))
         self.workspace = Workspace(self.test_dir)
+
         async def sample_fn():
             if self.workspace.get_load_path() is not None:
                 raise Exception("Checkpoint not loaded")
@@ -79,11 +86,14 @@ class TestWorkspace(unittest.IsolatedAsyncioTestCase):
         result = await self.workspace.with_checkpoints(sample_fn)
         self.assertEqual(result, None)
         self.assertEqual(self.workspace.current_load_checkpoint, None)
-        self.assertEqual(self.workspace.failed_checkpoints, ['3', '2', '1'])
+        self.assertEqual(self.workspace.failed_checkpoints, ["3", "2", "1"])
+
 
 class TestNamespace(unittest.TestCase):
     def setUp(self):
-        def _(self: Workspace) -> None: pass
+        def _(self: Workspace) -> None:
+            pass
+
         Workspace.__del__ = _
 
     def test_get_load_path_no_checkpoint(self):
@@ -91,7 +101,9 @@ class TestNamespace(unittest.TestCase):
         self.workspace = Workspace(self.test_dir)
         self.workspace.__del__ = lambda: None
         self.namespace = Namespace(self.workspace, "test_namespace")
-        self.assertEqual("test_workspace\\test_namespace_resource", self.namespace.get_load_path("resource"))
+        self.assertEqual(
+            os.path.join("test_workspace", "test_namespace_resource"), self.namespace.get_load_path("resource")
+        )
         del self.workspace
         gc.collect()
         if os.path.exists(self.test_dir):
@@ -115,12 +127,13 @@ class TestNamespace(unittest.TestCase):
         self.workspace = Workspace(self.test_dir)
         self.namespace = Namespace(self.workspace, "test_namespace")
         save_path = self.namespace.get_save_path("resource")
-        self.assertTrue(os.path.exists(save_path.rsplit("\\", 1)[0]))
+        self.assertTrue(os.path.exists(os.path.join(*os.path.split(save_path)[:-1])))
 
         del self.workspace
         gc.collect()
         if os.path.exists(self.test_dir):
             shutil.rmtree(self.test_dir)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
