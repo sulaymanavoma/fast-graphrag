948b9a19537fb16aae83886def42e9ef6ec24ef7
Luca Pinchetti <luca.pinchetti@cs.ox.ac.uk>
2024-11-19 09:23:09 -0800

    Remove ollama wrapper

--- fast_graphrag/_llm/__init__.py
+++ fast_graphrag/_llm/__init__.py
@@ -4,13 +4,10 @@ __all__ = [
     "DefaultEmbeddingService",
     "DefaultLLMService",
     "format_and_send_prompt",
-    "OllamaAIEmbeddingService",
-    "OllamaAILLMService",
     "OpenAIEmbeddingService",
     "OpenAILLMService",
 ]
 
 from ._base import BaseEmbeddingService, BaseLLMService, format_and_send_prompt
 from ._default import DefaultEmbeddingService, DefaultLLMService
-from ._llm_ollama import OllamaAIEmbeddingService, OllamaAILLMService
 from ._llm_openai import OpenAIEmbeddingService, OpenAILLMService
