d1230f52527a5f90f6eeab100ec7a7434cff7b61
Elliot Huang <elliot.huang@vantiva.com>
2024-11-19 15:04:02 +0800

    openai: support override base_url

--- fast_graphrag/_llm/_llm_openai.py
+++ fast_graphrag/_llm/_llm_openai.py
@@ -31,7 +31,7 @@ class OpenAILLMService(BaseLLMService):
 
     def __post_init__(self):
         logger.debug("Initialized OpenAILLMService with patched OpenAI client.")
-        self.llm_async_client: instructor.AsyncInstructor = instructor.from_openai(AsyncOpenAI(api_key=self.api_key))
+        self.llm_async_client: instructor.AsyncInstructor = instructor.from_openai(AsyncOpenAI(api_key=self.api_key, base_url=self.base_url))
 
     @retry(
         stop=stop_after_attempt(3),
@@ -112,7 +112,7 @@ class OpenAIEmbeddingService(BaseEmbeddingService):
     model: Optional[str] = field(default="text-embedding-3-small")
 
     def __post_init__(self):
-        self.embedding_async_client: AsyncOpenAI = AsyncOpenAI(api_key=self.api_key)
+        self.embedding_async_client: AsyncOpenAI = AsyncOpenAI(api_key=self.api_key, base_url=self.base_url)
         logger.debug("Initialized OpenAIEmbeddingService with OpenAI client.")
 
     @retry(
